23ca90ba4b1c822c58fcf52b56d839fe2092407b
Andrew <38564365+fastb1t@users.noreply.github.com>
2019-02-07 17:08:11 +0200

    Added test program and test DLL

---
+++ Solution/TestDll/main.cpp
@@ -0,0 +1,83 @@
+#include <tchar.h>
+#include <windows.h>
+
+static HWND g_hWnd = NULL;
+static HANDLE hThread = NULL;
+
+// [EnumWindowsCallback]:
+static BOOL CALLBACK EnumWindowsCallback(HWND hWnd, LPARAM lParam)
+{
+    DWORD dwPID = 0;
+    if (GetWindowThreadProcessId(hWnd, &dwPID))
+    {
+        if (dwPID == lParam)
+        {
+            g_hWnd = hWnd;
+            return FALSE;
+        }
+    }
+    return TRUE;
+}
+// [/EnumWindowsCallback]
+
+// [Init_Thread]:
+static DWORD WINAPI Init_Thread(LPVOID lpObject)
+{
+    HANDLE hProcess = GetCurrentProcess();
+    if (hProcess)
+    {
+        DWORD dwPID = GetProcessId(hProcess);
+        if (dwPID)
+        {
+            if (!EnumWindows(&EnumWindowsCallback, dwPID))
+            {
+                HDC hDC = GetDC(g_hWnd);
+                if (hDC)
+                {
+                    RECT rc;
+                    GetClientRect(g_hWnd, &rc);
+                    FillRect(hDC, &rc, (HBRUSH)GetStockObject(LTGRAY_BRUSH));
+                    int iOldBkMode = SetBkMode(hDC, TRANSPARENT);
+                    COLORREF clrOldColor = SetTextColor(hDC, RGB(255, 0, 0));
+                    const TCHAR szText[] = _T("We are hacked!");
+                    DrawText(hDC, szText, lstrlen(szText), &rc, DT_CENTER | DT_VCENTER);
+                    SetTextColor(hDC, clrOldColor);
+                    SetBkMode(hDC, iOldBkMode);
+                    ReleaseDC(g_hWnd, hDC);
+                }
+                MessageBox(g_hWnd, _T("Oops..."), _T("Oops..."), MB_OK | MB_ICONINFORMATION | MB_TOPMOST);
+            }
+        }
+    }
+
+    ExitThread(1337);
+}
+// [/Init_Thread]
+
+
+// [DllMain]: entry point.
+BOOL APIENTRY DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved)
+{
+    switch (dwReason)
+    {
+    case DLL_PROCESS_ATTACH:
+    {
+        DWORD dwThreadId = 0;
+        hThread = CreateThread(NULL, 0, Init_Thread, NULL, 0, &dwThreadId);
+    }
+    break;
+
+    case DLL_PROCESS_DETACH:
+    {
+        if (hThread)
+            CloseHandle(hThread);
+    }
+    break;
+
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+        break;
+    }
+    return TRUE;
+}
+// [/DllMain]
